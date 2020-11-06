bb3774a4a577bcf8da26455764332b0a7fb98eb7
Abdullah-MGK <abdullah_mgk@outlook.com>
2020-11-06 23:20:02 +0300

    add missing imports

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,10 +1,11 @@
 import os
+import sys
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 #from flask_cors import CORS
 import random
 
-from models import setup_db, Question, Category
+from models import setup_db, Question, Category, db
 
 
 
